9dff4126b143dd1343031ace4563c25d7bf6826f
astrofrog <thomas.robitaille@gmail.com>
2009-09-18 16:00:51 +0000

    - bumped version number to 0.9.1 in advance
    git-svn-id: https://atpy.svn.sourceforge.net/svnroot/atpy/trunk/src/ATpy@86 a84d3894-9e97-4a94-862a-515d26404f57

--- atpy/__init__.py
+++ atpy/__init__.py
@@ -1,2 +1,2 @@
 from basetable import Table, TableSet, VectorException
-__version__ = '0.9.0'
+__version__ = '0.9.1'
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='ATpy',
-      version='0.9.0',
+      version='0.9.1',
       description='Astronomical Tables in Python',
       author='Eli Bressert and Thomas Robitaille',
       author_email='elibre@users.sourceforge.net, \
