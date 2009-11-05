5e3e22aa1c46a32049fe3b8734420470bf7a8bce
astrofrog <thomas.robitaille@gmail.com>
2009-11-05 00:09:25 +0000

    Added vector columns for FITS and VO to unit tests
    git-svn-id: https://atpy.svn.sourceforge.net/svnroot/atpy/trunk/src/ATpy@125 a84d3894-9e97-4a94-862a-515d26404f57

--- test/unittests.py
+++ test/unittests.py
@@ -9,20 +9,23 @@ import atpy
 import numpy as np
 
 # Size of the test table
-SIZE = 100
+shape = (100,)
+shape_vector = (100,10)
 
 # SQL connection parameters
 username = getpass.getuser()
 password = "C2#uwQsk"
 
 
-def random_int_array(dtype, n):
+def random_int_array(dtype, shape):
+    n = np.product(shape)
     n = n * np.iinfo(dtype).bits / 8
     s = "".join(chr(random.randrange(0, 256)) for i in xrange(n))
-    return np.fromstring(s, dtype=dtype)
+    return np.fromstring(s, dtype=dtype).reshape(shape)
 
 
-def random_float_array(dtype, n):
+def random_float_array(dtype, shape):
+    n = np.product(shape)
     if dtype == np.float32:
         n = n * 4
     else:
@@ -30,8 +33,9 @@ def random_float_array(dtype, n):
     s = "".join(chr(random.randrange(0, 256)) for i in xrange(n))
     array = np.fromstring(s, dtype=dtype)
     if np.sum(np.isnan(array)):
-        array[np.isnan(array)] = random_float_array(dtype, np.sum(np.isnan(array)))
-    return array
+        array[np.isnan(array)] = random_float_array(dtype, array[np.isnan(array)].shape)
+    return array.reshape(shape)
+
 
 numpy_types = [np.bool, np.int8, np.int16, np.int32, np.int64, np.uint8, \
                np.uint16, np.uint32, np.uint64, np.float32, np.float64, \
@@ -57,17 +61,17 @@ def random_uint64(size):
     return a
 
 
-def random_generic(dtype, name, n):
+def random_generic(dtype, name, shape):
 
     if 'int' in name:
-        values = random_int_array(dtype, n)
+        values = random_int_array(dtype, shape)
     elif 'float' in name:
-        values = random_float_array(dtype, n)
+        values = random_float_array(dtype, shape)
     elif 'bool' in name:
-        values = np.random.random(n) > 0.5
+        values = np.random.random(shape) > 0.5
     else:
-        values = np.zeros((n, ), dtype=dtype)
-        for i in range(n):
+        values = np.zeros(shape, dtype=dtype)
+        for i in range(shape[0]):
             s = ""
             for k in range(dtype.itemsize):
                 s += random.choice(string.letters + string.digits)
@@ -94,7 +98,7 @@ def generate_table():
     return table
 
 
-def generate_simple_table(dtype):
+def generate_simple_table(dtype, shape):
 
     table = atpy.Table(name='atpy_test')
 
@@ -103,7 +107,7 @@ def generate_simple_table(dtype):
     except:
         name = dtype.type.__name__
 
-    values = random_generic(dtype, name, SIZE)
+    values = random_generic(dtype, name, shape)
 
     table.add_column('col_'+name, values, dtype=dtype)
 
@@ -118,97 +122,88 @@ class DefaultTestCase():
             raise test.failureException, \
             (msg or '%r != %r within %r significant digits' % (first, second, significant))
 
-    def test_uint8(self):
-        self.writeread(np.uint8)
-        before, after = self.table_orig.data['col_uint8'], self.table_new.data['col_uint8']
+    def integer_test(self, dtype):
+        colname = 'col_%s' % dtype.__name__
+        self.writeread(dtype)
+        before, after = self.table_orig.data[colname], self.table_new.data[colname]
         self.assertEqual(before.shape, after.shape)
-        for i in range(len(self.table_orig)):
-            self.assertEqual(before[i], after[i])
+        if before.ndim == 1:
+            for i in range(before.shape[0]):
+                self.assertEqual(before[i], after[i])
+        else:
+            for i in range(before.shape[0]):
+                for j in range(before.shape[1]):
+                    self.assertEqual(before[i,j], after[i,j])
+
+    def test_uint8(self):
+        self.integer_test(np.uint8)
 
     def test_uint16(self):
-        self.writeread(np.uint16)
-        before, after = self.table_orig.data['col_uint16'], self.table_new.data['col_uint16']
-        self.assertEqual(before.shape, after.shape)
-        for i in range(len(self.table_orig)):
-            self.assertEqual(before[i], after[i])
+        self.integer_test(np.uint16)
 
     def test_uint32(self):
-        self.writeread(np.uint32)
-        before, after = self.table_orig.data['col_uint32'], self.table_new.data['col_uint32']
-        self.assertEqual(before.shape, after.shape)
-        for i in range(len(self.table_orig)):
-            self.assertEqual(before[i], after[i])
+        self.integer_test(np.uint32)
 
     def test_uint64(self):
-        self.writeread(np.uint64)
-        before, after = self.table_orig.data['col_uint64'], self.table_new.data['col_uint64']
-        self.assertEqual(before.shape, after.shape)
-        for i in range(len(self.table_orig)):
-            self.assertEqual(before[i], after[i])
+        self.integer_test(np.uint64)
 
     def test_int8(self):
-        self.writeread(np.int8)
-        before, after = self.table_orig.data['col_int8'], self.table_new.data['col_int8']
-        self.assertEqual(before.shape, after.shape)
-        for i in range(len(self.table_orig)):
-            self.assertEqual(before[i], after[i])
+        self.integer_test(np.int8)
 
     def test_int16(self):
-        self.writeread(np.int16)
-        before, after = self.table_orig.data['col_int16'], self.table_new.data['col_int16']
-        self.assertEqual(before.shape, after.shape)
-        for i in range(len(self.table_orig)):
-            self.assertEqual(before[i], after[i])
+        self.integer_test(np.int16)
 
     def test_int32(self):
-        self.writeread(np.int32)
-        before, after = self.table_orig.data['col_int32'], self.table_new.data['col_int32']
-        self.assertEqual(before.shape, after.shape)
-        for i in range(len(self.table_orig)):
-            self.assertEqual(before[i], after[i])
+        self.integer_test(np.int32)
 
     def test_int64(self):
-        self.writeread(np.int64)
-        before, after = self.table_orig.data['col_int64'], self.table_new.data['col_int64']
+        self.integer_test(np.int64)
+
+    def float_test(self, dtype, significant=None):
+        colname = 'col_%s' % dtype.__name__
+        self.writeread(dtype)
+        before, after = self.table_orig.data[colname], self.table_new.data[colname]
         self.assertEqual(before.shape, after.shape)
-        for i in range(len(self.table_orig)):
-            self.assertEqual(before[i], after[i])
+        self.assertEqual(before.dtype.type, after.dtype.type)
+        if before.ndim == 1:
+            for i in range(before.shape[0]):
+                if(np.isnan(before[i])):
+                    self.failUnless(np.isnan(after[i]))
+                elif(np.isinf(before[i])):
+                    self.failUnless(np.isinf(after[i]))
+                else:
+                    if significant:
+                        self.assertAlmostEqualSig(before[i], after[i], significant=significant)
+                    else:
+                        self.assertEqual(before[i], after[i])
+        else:
+            for i in range(before.shape[0]):
+                for j in range(before.shape[1]):
+                    if(np.isnan(before[i,j])):
+                        self.failUnless(np.isnan(after[i,j]))
+                    elif(np.isinf(before[i,j])):
+                        self.failUnless(np.isinf(after[i,j]))
+                    else:
+                        if significant:
+                            self.assertAlmostEqualSig(before[i,j], after[i,j], significant=significant)
+                        else:
+                            self.assertEqual(before[i,j], after[i,j])
 
     def test_float32(self):
-        self.writeread(np.float32)
-        before, after = self.table_orig.data['col_float32'], self.table_new.data['col_float32']
-        self.assertEqual(before.shape, after.shape)
-        self.assertEqual(before.dtype.type, after.dtype.type)
-        for i in range(len(self.table_orig)):
-            if(np.isnan(before[i])):
-                self.failUnless(np.isnan(after[i]))
-            elif(np.isinf(before[i])):
-                self.failUnless(np.isinf(after[i]))
-            else:
-                if self.format == 'mysql':
-                    self.assertAlmostEqualSig(before[i], after[i], significant=5)
-                if self.format == 'postgres':
-                    self.assertAlmostEqualSig(before[i], after[i], significant=5)
-                else:
-                    self.assertEqual(before[i], after[i])
+        if self.format == 'mysql':
+            self.float_test(np.float32, significant=5)
+        if self.format == 'postgres':
+            self.float_test(np.float32, significant=5)
+        else:
+            self.float_test(np.float32)
 
     def test_float64(self):
-        self.writeread(np.float64)
-        before, after = self.table_orig.data['col_float64'], self.table_new.data['col_float64']
-        self.assertEqual(before.shape, after.shape)
-        self.assertEqual(before.dtype.type, after.dtype.type)
-        for i in range(len(self.table_orig)):
-            if(np.isnan(before[i])):
-                self.failUnless(np.isnan(after[i]))
-            elif(np.isinf(before[i])):
-                self.failUnless(np.isinf(after[i]))
-            else:
-                if self.format == 'mysql':
-                    self.assertAlmostEqualSig(before[i], after[i], significant=12)
-                elif self.format == 'postgres':
-                        self.assertAlmostEqualSig(before[i], after[i], significant=12)
-                else:
-                    self.assertEqual(before[i], after[i])
+        if self.format == 'mysql':
+            self.float_test(np.float64, significant=12)
+        if self.format == 'postgres':
+            self.float_test(np.float64, significant=12)
+        else:
+            self.float_test(np.float64)
 
     def test_string(self):
         self.writeread(np.dtype('|S100'))
@@ -227,7 +222,18 @@ try:
 
         def writeread(self, dtype):
 
-            self.table_orig = generate_simple_table(dtype)
+            self.table_orig = generate_simple_table(dtype, shape)
+            self.table_orig.write('test_atpy.fits', verbose=False, overwrite=True)
+            self.table_new = atpy.Table('test_atpy.fits', verbose=False)
+            os.remove('test_atpy.fits')
+
+    class FITSTestCaseVector(unittest.TestCase, DefaultTestCase):
+
+        format = 'fits'
+
+        def writeread(self, dtype):
+
+            self.table_orig = generate_simple_table(dtype, shape_vector)
             self.table_orig.write('test_atpy.fits', verbose=False, overwrite=True)
             self.table_new = atpy.Table('test_atpy.fits', verbose=False)
             os.remove('test_atpy.fits')
@@ -246,7 +252,20 @@ try:
 
         def writeread(self, dtype):
 
-            self.table_orig = generate_simple_table(dtype)
+            self.table_orig = generate_simple_table(dtype, shape)
+            self.table_orig.write('test_atpy.xml', verbose=False)
+            self.table_new = atpy.Table('test_atpy.xml', verbose=False)
+            os.remove('test_atpy.xml')
+
+    class VOTestCaseVector(unittest.TestCase, DefaultTestCase):
+
+        format = 'vo'
+
+        test_uint64 = None # unsupported
+
+        def writeread(self, dtype):
+
+            self.table_orig = generate_simple_table(dtype, shape_vector)
             self.table_orig.write('test_atpy.xml', verbose=False)
             self.table_new = atpy.Table('test_atpy.xml', verbose=False)
             os.remove('test_atpy.xml')
@@ -261,7 +280,7 @@ class IPACTestCase(unittest.TestCase, DefaultTestCase):
 
     def writeread(self, dtype):
 
-        self.table_orig = generate_simple_table(dtype)
+        self.table_orig = generate_simple_table(dtype, shape)
         self.table_orig.write('test_atpy.tbl', verbose=False, overwrite=True)
         self.table_new = atpy.Table('test_atpy.tbl', verbose=False)
         os.remove('test_atpy.tbl')
@@ -277,7 +296,7 @@ try:
 
         def writeread(self, dtype):
 
-            self.table_orig = generate_simple_table(dtype)
+            self.table_orig = generate_simple_table(dtype, shape)
             self.table_orig.write('sqlite', 'test_atpy.db', verbose=False, overwrite=True)
             self.table_new = atpy.Table('sqlite', 'test_atpy.db', verbose=False)
             os.remove('test_atpy.db')
@@ -294,7 +313,7 @@ try:
 
         def writeread(self, dtype):
 
-            self.table_orig = generate_simple_table(dtype)
+            self.table_orig = generate_simple_table(dtype, shape)
             self.table_orig.write('mysql', db='python', overwrite=True, verbose=False, user=username, passwd=password)
             self.table_new = atpy.Table('mysql', db='python', verbose=False, user=username, passwd=password, table='atpy_test')
 
@@ -304,7 +323,7 @@ try:
 
         def writeread(self, dtype):
 
-            self.table_orig = generate_simple_table(dtype)
+            self.table_orig = generate_simple_table(dtype, shape)
             self.table_orig.write('mysql', db='python', overwrite=True, verbose=False, user=username, passwd=password)
             self.table_new = atpy.Table('mysql', db='python', verbose=False, user=username, passwd=password, table='atpy_test', query='select * from atpy_test')
 
@@ -322,7 +341,7 @@ try:
 
         def writeread(self, dtype):
 
-            self.table_orig = generate_simple_table(dtype)
+            self.table_orig = generate_simple_table(dtype, shape)
             self.table_orig.write('postgres', database='python', overwrite=True, verbose=False, user=username, password=password)
             self.table_new = atpy.Table('postgres', database='python', verbose=False, user=username, password=password, table='atpy_test')
 
@@ -334,7 +353,7 @@ try:
 
         def writeread(self, dtype):
 
-            self.table_orig = generate_simple_table(dtype)
+            self.table_orig = generate_simple_table(dtype, shape)
             self.table_orig.write('postgres', database='python', overwrite=True, verbose=False, user=username, password=password)
             self.table_new = atpy.Table('postgres', database='python', verbose=False, user=username, password=password, table='atpy_test', query='select * from atpy_test')
 
