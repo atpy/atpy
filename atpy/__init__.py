a3472d20c9fa093dea208ad44d711626f25ddf50
ebressert <ebressert@cfa.harvard.edu>
2010-10-21 10:35:39 +0000

    __version__ text is updated (thanks to Moritz for catching it).
    git-svn-id: https://atpy.svn.sourceforge.net/svnroot/atpy/trunk/src/ATpy@239 a84d3894-9e97-4a94-862a-515d26404f57

--- atpy/__init__.py
+++ atpy/__init__.py
@@ -5,7 +5,7 @@ from ConfigParser import SafeConfigParser
 import atpy
 from basetable import Table, TableSet, VectorException
 
-__version__ = '0.9.3'
+__version__ = '0.9.4'
 
 _readers = {}
 _writers = {}
